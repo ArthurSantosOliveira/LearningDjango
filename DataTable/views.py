6e3241677dbd7b9a8884921446f317b8bf37658e
Arthur Santos Oliveria <pessoalarthursoliveira@gmail.com>
2023-09-13 21:17:29 -0300

    paginação funcionando
    entretanto ao selecionar de Z-A e clicar em próxima ele perde e volta para A-Z

--- DataTable/views.py
+++ DataTable/views.py
@@ -1,35 +1,49 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.http import QueryDict
-from .models import Funcionario
 from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import Funcionario
 from .forms import BuscaForm, OrdenacaoForm
 
 def mainDataTable(request):
-    registers = Funcionario.objects.all()
-    # Crie uma instância dos formulários
-    busca_form = BuscaForm()  
-    ordenacao_form = OrdenacaoForm()
-    if request.GET.get('termo_busca'):
-        termo_busca = request.GET['termo_busca']
-        registers = registers.filter(nome=termo_busca)
-
-    return render(request, 'mainDataTable.html', {'registers': registers, 'busca_form': busca_form,'ordenacao_form': ordenacao_form})
-
-def ordenarFuncionarios(request):
-    #obtem valor padrao
-    ordenacao = request.GET.get('ordenacao', 'asc')  
+    # registros de Funcionario
     registros = Funcionario.objects.all()
 
-    #ordena os campos de acordo com a escolha
-    if ordenacao == 'asc':
-        registros = registros.order_by('nome')  
-    elif ordenacao == 'desc':
-        registros = registros.order_by('-nome') 
-    else:
-        registros = Funcionario.objects.all()
+    # instância dos formulários
+    busca_form = BuscaForm()
+    ordenacao_form = OrdenacaoForm()
 
+    # número de registros a serem exibidos por página
+    registros_por_pagina = 10
+    paginator = Paginator(registros, registros_por_pagina)
+    pagina_numero = request.GET.get('page')
+    pagina_atual = paginator.get_page(pagina_numero)
+
+    # pesquisa
+    termo_busca = request.GET.get('termo_busca')
+    if termo_busca:
+        registros = registros.filter(nome__icontains=termo_busca)
+
+    return render(request, 'mainDataTable.html', {'pagina_atual': pagina_atual, 'busca_form': busca_form, 'ordenacao_form': ordenacao_form})
+
+def ordenarFuncionarios(request):
+    # pega o valor do GET
+    ordenacao = request.GET.get('ordenacao', 'asc')
     termo_busca = request.GET.get('termo_busca', '')
+
+    registros = Funcionario.objects.all()
+
+    # ordena os registros
+    if ordenacao == 'asc':
+        registros = registros.order_by('nome')
+    elif ordenacao == 'desc':
+        registros = registros.order_by('-nome')
+
+    # filtra os registros
     registros = registros.filter(nome__icontains=termo_busca)
 
-    return render(request, 'mainDataTable.html', {'registers': registros, 'busca_form': BuscaForm({'termo_busca': termo_busca}), 'ordenacao_form': OrdenacaoForm({'ordenacao': ordenacao})})
+    # número de registros a serem exibidos por página
+    registros_por_pagina = 10
+    paginator = Paginator(registros, registros_por_pagina)
+    pagina_numero = request.GET.get('page')
+    pagina_atual = paginator.get_page(pagina_numero)
+
+    return render(request, 'mainDataTable.html', {'pagina_atual': pagina_atual, 'busca_form': BuscaForm({'termo_busca': termo_busca}), 'ordenacao_form': OrdenacaoForm({'ordenacao': ordenacao})})
