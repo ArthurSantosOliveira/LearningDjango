816a0fcacf60dccabe367b6d1f536e16f19f4c54
Arthur Santos Oliveria <pessoalarthursoliveira@gmail.com>
2023-09-17 21:13:18 -0300

    teste

--- DataTable/views.py
+++ DataTable/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from .models import Funcionario
 from .models import Funcionario
 
-
+#teste push e commit via git
 
 def ordenarFuncionarios(request):
     registros = Funcionario.objects.all()
-
+    print('teste')
     return render(request, 'mainDataTable.html', { 'registros':registros})
